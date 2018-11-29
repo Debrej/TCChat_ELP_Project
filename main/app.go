37e2e45414f51e13c77338774a6874172a8256a6
Debrej <t.bellanger34@gmail.com>
2018-11-29 12:23:49 +0100

    Adding json file for ServerMessage ParseServer sending parameters of the messages

--- main/app.go
+++ main/app.go
@@ -10,6 +10,9 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter text: ")
 	testString, _ := reader.ReadString('\n')
-	msg := ParseServer(testString)
-	fmt.Print(msg)
+
+	msgParams := ParseServer(testString)
+	for i := 0; i < len(msgParams); i++ {
+		fmt.Print(msgParams[i])
+	}
 }
--- main/message_parser.go
+++ main/message_parser.go
@@ -1,12 +1,55 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
 	"strings"
 )
 
-func ParseServer(str string) string {
+type MessageArray struct {
+	Messages []Message `json:"messages"`
+}
+
+type Message struct {
+	Name       string   `json:"name"`
+	Parameters []string `json:"parameters"`
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func parseJSON(str string) []Message {
+	decoderJson := json.NewDecoder(strings.NewReader(str))
+	var mA MessageArray
+	errA := decoderJson.Decode(&mA)
+	check(errA)
+	return mA.Messages
+}
+
+func ParseServer(str string) []string {
+	absPath, _ := filepath.Abs("./main/ServerMessages.json")
+	messagesArrayBytes, err := ioutil.ReadFile(absPath)
+	check(err)
+	messagesArray := string(messagesArrayBytes[:])
+	messages := parseJSON(messagesArray)
 	lenStr := len(str) - 1
 	tmpStr := str[:lenStr]
 	aStr := strings.Split(tmpStr, "\t")
-	return aStr[0]
+	msgName := aStr[0]
+	found := false
+	i := 0
+	var retParam []string
+	for !found {
+		if messages[i].Name == msgName {
+			found = true
+			retParam = messages[i].Parameters
+		} else {
+			i++
+		}
+	}
+	return retParam
 }
