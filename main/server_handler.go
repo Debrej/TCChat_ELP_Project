83cc1fbec56d290890fe2ef65224e5a1a2f4ac51
Debrej <t.bellanger34@gmail.com>
2018-12-05 15:50:10 +0100

    Client server tests

--- main/server_handler.go
+++ main/server_handler.go
@@ -1,24 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"strconv"
 )
 
-func ServerHandler(msgName string, msgParams map[string]string, users map[int]string) (map[int]string, string) {
+func ServerHandler(msgName string, msgParams map[string]string, conn net.Conn, users map[int]string, conns map[int]net.Conn) (map[int]string, map[int]net.Conn, string) {
 	var msg string
 
 	switch msgName {
 	case "TCCHAT_REGISTER", "TCCHAT_DISCONNECT":
-		users, msg = serverUserHandler(msgName, msgParams, users)
+		users, conns, msg = serverUserHandler(msgName, msgParams, conn, users, conns)
 
 	case "TCCHAT_MESSAGE":
 		users, msg = serverMessageHandler(msgParams, users)
 	}
 
-	return users, msg
+	return users, conns, msg
 }
 
-func serverUserHandler(msgName string, msgParams map[string]string, users map[int]string) (map[int]string, string) {
+func serverUserHandler(msgName string, msgParams map[string]string, conn net.Conn, users map[int]string, conns map[int]net.Conn) (map[int]string, map[int]net.Conn, string) {
 	var uid int
 	var msg string
 
@@ -34,16 +36,19 @@ func serverUserHandler(msgName string, msgParams map[string]string, users map[in
 			}
 		}
 		users[i] = msgParams["nickname"]
-		msg = "TCCHAT_USERIN\t" + msgParams["nickname"]
+		conns[i] = conn
+		msg = "TCCHAT_WELCOME\tELP_TCCHAT\t" + strconv.Itoa(i) + "\n"
+		fmt.Println(msg)
 		uid = i
 
 	case "TCCHAT_DISCONNECT":
 		uid, _ = strconv.Atoi(msgParams["uid"])
-		msg = "TCCHAT_USEROUT\t" + users[uid]
+		msg = "TCCHAT_USEROUT\t" + users[uid] + "\n"
 		delete(users, uid)
+		delete(conns, uid)
 
 	}
-	return users, msg
+	return users, conns, msg
 }
 
 func serverMessageHandler(msgParams map[string]string, users map[int]string) (map[int]string, string) {
@@ -51,9 +56,9 @@ func serverMessageHandler(msgParams map[string]string, users map[int]string) (ma
 	var retString string
 	uid, _ := strconv.Atoi(msgParams["uid"])
 	if isPersonal {
-		retString = "TCCHAT_PERSONAL\t" + msg + "\t" + users[uid] + "\t" + dest
+		retString = "TCCHAT_PERSONAL\t" + msg + "\t" + users[uid] + "\t" + dest + "\n"
 	} else {
-		retString = "TCCHAT_MESSAGE\t" + msg + "\t" + users[uid]
+		retString = "TCCHAT_MESSAGE\t" + msg + "\t" + users[uid] + "\n"
 	}
 	return users, retString
 }
