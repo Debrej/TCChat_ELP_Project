2a1466d814bc334082c85bdd004636a4cbdcfc65
clement <clement.vinciguerra@insa-lyon.fr>
2018-12-05 15:51:37 +0100

    Récupération Nickname envoi TCP

--- main/client.go
+++ main/client.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	conn, errDial := net.Dial("tcp", "golang.org:80")
+
+	nickname := Read("Please enter your nickname : ")
+	msg_nickname := "TCCHAT_REGISTER\t" + nickname + "\n"
+
+	conn, errDial := net.Dial("tcp", "192.168.43.10:16000")
 	check(errDial)
+	_, errWrite := conn.Write([]byte(msg_nickname))
+	check(errWrite)
+
 	msg, errRead := bufio.NewReader(conn).ReadString('\n')
 	check(errRead)
+
 	msgCName, _, msgCParams := ParseClient(msg)
 	ClientHandler(msgCName, msgCParams)
 }
